355de58f2df31bb77ba2cafd54f57b2c9318b1ee
Everton Pereira <suporte@agencialink.com>
2026-07-23 17:16:08 -0300

    Definindo quantidade de dias após o vencimento que o pagamento será aceito

--- src/Itau/API/Pix/Calendario.php
+++ src/Itau/API/Pix/Calendario.php
@@ -9,6 +9,7 @@ class Calendario implements \JsonSerializable
     use TraitEntity;
 
     private int $expiracao = 86400;
+    private int $validadeAposVencimento = '';
     private string $dataDeVencimento = '';
 
     public function setExpiracao(string $expiracao): self
@@ -17,9 +18,10 @@ class Calendario implements \JsonSerializable
         return $this;
     }
     
-    public function setVencimento(string $vencimento): self
+    public function setVencimento(string $vencimento, int $dias = 0): self
     {
         $this->dataDeVencimento = $vencimento;
+        $this->validadeAposVencimento = $dias;
         unset($this->expiracao);
         return $this;
     }
