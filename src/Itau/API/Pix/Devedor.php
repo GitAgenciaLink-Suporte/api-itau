fb171a67f924193ac3ec1a95c5b1a7fd99975e3f
Everton Pereira <suporte@agencialink.com>
2026-07-23 16:58:50 -0300

    Informando somente os campos obrigatórios

--- src/Itau/API/Pix/Devedor.php
+++ src/Itau/API/Pix/Devedor.php
@@ -8,30 +8,36 @@ class Devedor implements \JsonSerializable
 {
     use TraitEntity;
 
+    private string $nome        = '';
+    private string $cpf         = '';
+    private string $cnpj        = '';
     private string $email       = '';
     private string $logradouro  = '';
     private string $cidade      = '';
     private string $uf          = '';
     private string $cep         = '';
-    private string $cpf         = '';
-    private string $cnpj        = '';
-    private string $nome        = '';
 
     public function __construct(array $dados)
     {
-        $this->email      = $dados['email'];
-        $this->logradouro = $dados['logradouro'];
-        $this->cidade     = $dados['cidade'];
-        $this->uf         = $dados['uf'];
-        $this->cep        = preg_replace('/[^0-9]/', '', $dados['cep']);
-        $this->nome       = $dados['nome'];
-        
+        //apenas nome e cpf/cnpj são obrigatórios
+        $this->nome = $dados['nome'];
         if($dados['tipo_documento'] == 'cpf') {
             $this->cpf = preg_replace('/[^0-9]/', '', $dados['documento']);
-            unset($this->cnpj);
         } else {
             $this->cnpj = preg_replace('/[^a-zA-Z0-9]/', '', $dados['documento']);
-            unset($this->cpf);
+        }
+        
+        $this->email      = $dados['email'] ?? '';
+        $this->logradouro = $dados['logradouro'] ?? '';
+        $this->cidade     = $dados['cidade'] ?? '';
+        $this->uf         = $dados['uf'] ?? '';
+        $this->cep        = $dados['cep'] ?? '';
+        
+        //remover atributos vazios
+        foreach ($this as $key => $value) {
+            if($value == '') {
+                unset($this->$key);
+            }
         }
     }
 }
